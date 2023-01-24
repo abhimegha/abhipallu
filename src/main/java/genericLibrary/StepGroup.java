a206b7ef8646c1b865cdead6d7fd1d68c3b259d0
Admin <Admin@DESKTOP-G092QH2>
2023-01-24 13:34:51 +0530

    sample

--- src/main/java/genericLibrary/StepGroup.java
+++ src/main/java/genericLibrary/StepGroup.java
@@ -1,6 +1,15 @@
 package genericLibrary;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class StepGroup {
 	public WebDriver driver;
@@ -9,8 +18,27 @@ public class StepGroup {
 	
 	@BeforeMethod
 	
-	public void openApp() {
+	public void openApp() throws IOException {
 		
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(pdata.getPropertydata("url"));
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		
+		
+	}
+	@AfterMethod
+	public void closeApp(ITestResult res) throws IOException {
+		
+		int status = res.getStatus();
+		String name = res.getName();
+		
+		if(status==2) {
+			Screenshot s=new Screenshot();
+			s.getScreenshot(driver, name);
+		}
+		 driver.quit();
 	}
 	
 
